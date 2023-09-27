b1e5c6e21b8c34fd3e91cbfaa76c9bfaa34716ef
Gonza1065 <104340268+Gonza1065@users.noreply.github.com>
2023-09-27 14:18:39 -0300

    Animación en el menu de despliegue

--- src/components/NavBarPresentation.jsx
+++ src/components/NavBarPresentation.jsx
@@ -5,12 +5,15 @@ import { NavLink } from "react-router-dom";
 import "../componentsCss/NavBarPresentation.css";
 export function NavBarPresentation() {
   const [isOpenMenuMobile, setIsOpenMenuMobile] = useState(false);
+  const [animationBars, setAnimationBars] = useState(false);
   const toggleMenuMobile = () => {
     setIsOpenMenuMobile(!isOpenMenuMobile);
+    setAnimationBars(!animationBars);
   };
   const classNameMobile = isOpenMenuMobile
     ? "menu-mobile-open"
     : "menu-mobile-close";
+  const classNameAnimationBars = animationBars ? "animation-active" : "";
   return (
     <>
       <header className="logo-navbar">
@@ -49,43 +52,43 @@ export function NavBarPresentation() {
           </ul>
         </nav>
       </header>
-     
-        <header>
-          <nav>
-            <ul className={classNameMobile}>
-              <li>
-                <NavLink to="/">Inicio</NavLink>
-              </li>
-              <li>
-                <NavLink to="/invitacion">Invitación</NavLink>
-              </li>
-              <li>
-                <NavLink to="/nuestra-historia">Nuestra Historia</NavLink>
-              </li>
-              <li>
-                <NavLink to="/iglesia">Iglesia</NavLink>
-              </li>
-              <li>
-                <NavLink to="/fiesta">Fiesta</NavLink>
-              </li>
-              <li>
-                <NavLink to="/musica">Musica</NavLink>
-              </li>
-              <li>
-                <NavLink to="/regalo">Regalo</NavLink>
-              </li>
-              <li>
-                <NavLink to="/galeria">Galería</NavLink>
-              </li>
-            </ul>
-          </nav>
-        </header>
-     
+
+      <header>
+        <nav>
+          <ul className={classNameMobile}>
+            <li>
+              <NavLink to="/">Inicio</NavLink>
+            </li>
+            <li>
+              <NavLink to="/invitacion">Invitación</NavLink>
+            </li>
+            <li>
+              <NavLink to="/nuestra-historia">Nuestra Historia</NavLink>
+            </li>
+            <li>
+              <NavLink to="/iglesia">Iglesia</NavLink>
+            </li>
+            <li>
+              <NavLink to="/fiesta">Fiesta</NavLink>
+            </li>
+            <li>
+              <NavLink to="/musica">Musica</NavLink>
+            </li>
+            <li>
+              <NavLink to="/regalo">Regalo</NavLink>
+            </li>
+            <li>
+              <NavLink to="/galeria">Galería</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
       <div className="menu-icon">
         <FontAwesomeIcon
           icon={faBars}
           onClick={toggleMenuMobile}
-          className="menu-icon"
+          className={`menu-icon ${classNameAnimationBars}`}
         />
       </div>
     </>
